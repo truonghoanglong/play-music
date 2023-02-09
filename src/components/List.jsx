92baa7ab80b16c66f1f559d72c45431e8c60381c
truonghoanglong <57944514+truonghoanglong@users.noreply.github.com>
2023-02-09 22:57:22 +0700

    'css'

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,14 +1,38 @@
-import React from 'react'
-import '../assets/css/list.css'
+import React from 'react';
+import '../assets/css/list.css';
+import music from '../assets/data';
 const List = ({props:{open,setOpen,musicNumber,setMusicNumber}}) => {
   return (
-    <div>
+    
         <div className="list">
             <div className="header">
+              <div className="">
                 <i className="material-icons">queue_music</i>
+                <span>Music list</span>
+                <i className="material-icons">close</i>
+              </div>
             </div>
+
+        <ul>
+          {
+            music.map((music,index)=>(
+              <li 
+                key={music.id} 
+                onClick={() => setMusicNumber(index)}
+                className={`${musicNumber === index ? 'play' : ''}`}
+              >
+                <div className="row">
+                  <span>{music.title}</span>
+                  <p>{music.artist}</p>
+                </div>
+                <span className='duration'>03:32</span>
+              </li>
+            ))
+          }
+        </ul>
+
         </div>
-    </div>
+
   )
 }
 
